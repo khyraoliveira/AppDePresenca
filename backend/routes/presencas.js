d80f9b4c5af2db6d96d14fdf8b91192449e2c97e
Khyra de Oliveira <khyrak@gmail.com>
2024-09-13 11:24:50 -0300

    Add pastas

---
+++ backend/routes/presencas.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const presencasController = require('../controllers/presencasController');
+
+router.get('/', presencasController.getAllPresencas);
+router.post('/', presencasController.createPresenca);
+router.put('/:id', presencasController.updatePresenca);
+router.delete('/:id', presencasController.deletePresenca);
+
+module.exports = router;
